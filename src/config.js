437dbefebf7fbb2e4c7738761a0c76ad97caff90
RABBITS BOTS <32734798+bayu2309@users.noreply.github.com>
2018-05-28 20:54:26 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  channelAccessToken: "",
-  channelSecret: ""
+  channelAccessToken: "QYdTGtbY+Aju/1Dp1ehXhodAdkyyI5Zrl1Gyx++FoFfmSTKS2sinAOLSBOFQjKtmqPAupZ52FUOWhSYica1xLw4j9MpuRX60iEqtgbaHlQGe0gg7RajE8PdpI4gnlyvFyg92ct8uK7m5k2sW6rDaKAdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "abd6f0ace80dd68f6a1133e4cfe68a09"
 };
 
 export default config;
